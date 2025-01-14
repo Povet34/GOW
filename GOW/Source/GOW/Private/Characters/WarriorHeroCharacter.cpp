d1e0ed5baed6a9bb0093fc744113adb3254fc3dc
Povet34 <70003696+Povet34@users.noreply.github.com>
2025-01-14 14:56:03 +0900

    SpringArm 장착

--- GOW/Source/GOW/Private/Characters/WarriorHeroCharacter.cpp
+++ GOW/Source/GOW/Private/Characters/WarriorHeroCharacter.cpp
@@ -1,5 +1,29 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
+#include "WarriorDebugHelper.h"
+#include "Components/CapsuleComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+
 #include "Characters/WarriorHeroCharacter.h"
 
+AWarriorHeroCharacter::AWarriorHeroCharacter()
+{
+	GetCapsuleComponent()->InitCapsuleSize(42.0f, 96.0f);
+
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationYaw = false;
+	bUseControllerRotationRoll = false;
+
+	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+	CameraBoom->SetupAttachment(GetRootComponent());
+	CameraBoom->TargetArmLength = 200.0f;
+	CameraBoom->SocketOffset = FVector(0.0f, 55.0f, 65.0f);
+	CameraBoom->bUsePawnControlRotation = true;
+}
+
+void AWarriorHeroCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	Debug::Print("Working");
+}
