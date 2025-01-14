86aeb5bbe01b371c187398dab891562b418bb26c
wotkd34 <wotkd34@naver.com>
2025-01-14 23:41:31 +0900

    GameplayTags 추가

---
+++ GOW/Source/GOW/Public/WarriorGameplayTags.h
@@ -0,0 +1,10 @@
+
+#pragma once
+
+#include "NativeGameplayTags.h"
+
+namespace WarriorGameplayTags
+{
+	GOW_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(InputTag_Move)
+	GOW_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(InputTag_Look)
+}
