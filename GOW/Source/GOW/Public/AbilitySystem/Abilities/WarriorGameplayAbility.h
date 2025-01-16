1e3bf75e8ef517c1a97ab50b65ed6de0080cae9a
Povet34 <70003696+Povet34@users.noreply.github.com>
2025-01-17 08:49:16 +0900

    어빌리티 웨폰생성

--- GOW/Source/GOW/Public/AbilitySystem/Abilities/WarriorGameplayAbility.h
+++ GOW/Source/GOW/Public/AbilitySystem/Abilities/WarriorGameplayAbility.h
@@ -22,6 +22,14 @@ class GOW_API UWarriorGameplayAbility : public UGameplayAbility
 	GENERATED_BODY()
 
 protected:
+
+	//Begin UGameplayAbility interface
+	virtual void OnGiveAbility(const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilitySpec& Spec) override;
+	virtual void EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled) override;
+
+
+	//End UGameplayAbility interface
+
 	UPROPERTY(EditDefaultsOnly, Category = "WarriorAbility")
 	EWarriorAbilityActivationPolicy AbilityActivationPolicy = EWarriorAbilityActivationPolicy::OnTriggered;
 };
