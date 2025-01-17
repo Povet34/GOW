b95f34cbf86d31b57302c5372cc180686d103512
Povet34 <70003696+Povet34@users.noreply.github.com>
2025-01-17 16:58:20 +0900

    Axe Equip Ability 구현중

---
+++ GOW/Source/GOW/Public/Itmes/Weapons/WarriorHeroWeapon.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Itmes/Weapons/WarriorWeaponBase.h"
+#include "WarriorHeroWeapon.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GOW_API AWarriorHeroWeapon : public AWarriorWeaponBase
+{
+	GENERATED_BODY()
+	
+};
