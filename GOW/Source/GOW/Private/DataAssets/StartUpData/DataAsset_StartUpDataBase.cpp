d502a8d2f74133e86d909e4dca24dec439cd4148
Povet34 <70003696+Povet34@users.noreply.github.com>
2025-01-19 13:43:33 +0900

    Startup 어빌리티 생성

---
+++ GOW/Source/GOW/Private/DataAssets/StartUpData/DataAsset_StartUpDataBase.cpp
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "DataAssets/StartUpData/DataAsset_StartUpDataBase.h"
+#include "AbilitySystem/WarriorAbilitySystemComponent.h"
+#include "AbilitySystem/Abilities/WarriorGameplayAbility.h"
+
+void UDataAsset_StartUpDataBase::GiveToAbilitySystemComponent(UWarriorAbilitySystemComponent* InWarriorASCToGive, int32 ApplyLevel)
+{
+	check(InWarriorASCToGive);
+
+	GrantAbilities(ActivateOnGivenAbilities, InWarriorASCToGive, ApplyLevel);	
+	GrantAbilities(ReactiveAbilities, InWarriorASCToGive, ApplyLevel);	
+}
+
+void UDataAsset_StartUpDataBase::GrantAbilities(const TArray<TSubclassOf<UWarriorGameplayAbility>>& Abilities, UWarriorAbilitySystemComponent* InWarriorASCToGive, int32 ApplyLevel)
+{
+	if (Abilities.IsEmpty()) 
+	{
+		return;
+	}
+
+	for (const TSubclassOf<UWarriorGameplayAbility>& Ability : Abilities)
+	{
+		if (!Ability)
+		{
+			continue;
+		}
+
+		FGameplayAbilitySpec AbilitySpec(Ability);
+		AbilitySpec.SourceObject = InWarriorASCToGive->GetAvatarActor();
+		AbilitySpec.Level = ApplyLevel;
+
+		InWarriorASCToGive->GiveAbility(AbilitySpec);
+	}
+}
