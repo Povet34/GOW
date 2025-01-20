cdab4ec925d2df0cf16771ff520d0fcd85c18119
Povet34 <70003696+Povet34@users.noreply.github.com>
2025-01-20 22:41:32 +0900

    HeroStartUP

--- GOW/Source/GOW/Private/DataAssets/StartUpData/DataAsset_HeroStartUpData.cpp
+++ GOW/Source/GOW/Private/DataAssets/StartUpData/DataAsset_HeroStartUpData.cpp
@@ -2,4 +2,30 @@
 
 
 #include "DataAssets/StartUpData/DataAsset_HeroStartUpData.h"
+#include "AbilitySystem/Abilities/WarriorGameplayAbility.h"
+#include "AbilitySystem/WarriorAbilitySystemComponent.h"
 
+bool FWarriorHeroAbilitySet::IsValid() const
+{
+    return InputTag.IsValid() && AbilityToGrant;
+}
+
+void UDataAsset_HeroStartUpData::GiveToAbilitySystemComponent(UWarriorAbilitySystemComponent* InASCToGive, int32 ApplyLevel)
+{
+	Super::GiveToAbilitySystemComponent(InASCToGive, ApplyLevel);
+
+	for (const FWarriorHeroAbilitySet& Ability : HeroStartUpAbilitySets)
+	{
+		if (!Ability.IsValid())
+		{
+			UE_LOG(LogTemp, Warning, TEXT("Invalid ability set in hero start up data"));
+			continue;
+		}
+
+		FGameplayAbilitySpec Spec(Ability.AbilityToGrant, ApplyLevel);
+		Spec.SourceObject = InASCToGive->GetAvatarActor();
+		Spec.DynamicAbilityTags.AddTag(Ability.InputTag);
+
+		InASCToGive->GiveAbility(Spec);
+	}
+}
