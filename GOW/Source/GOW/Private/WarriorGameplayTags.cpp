c22c0e2666b8b1174dc17875fc229d9c0447cdcf
Povet34 <70003696+Povet34@users.noreply.github.com>
2025-01-23 23:44:17 +0900

    ATTACK Tag 추가

--- GOW/Source/GOW/Private/WarriorGameplayTags.cpp
+++ GOW/Source/GOW/Private/WarriorGameplayTags.cpp
@@ -7,9 +7,14 @@ namespace WarriorGameplayTags
 	UE_DEFINE_GAMEPLAY_TAG(InputTag_Look, "InputTag.Look");
 	UE_DEFINE_GAMEPLAY_TAG(InputTag_EquipAxe, "InputTag.EquipAxe");
 	UE_DEFINE_GAMEPLAY_TAG(InputTag_UnequipAxe, "InputTag.UnequipAxe");
+	UE_DEFINE_GAMEPLAY_TAG(InputTag_LightAttack_Axe, "InputTag.UnequipAxe");
+	UE_DEFINE_GAMEPLAY_TAG(InputTag_HeavyAttack_Axe, "InputTag.LightAttack.Axe");
+	UE_DEFINE_GAMEPLAY_TAG(InputTag_HeavyAttack_Axe, "InputTag.HeavyAttack.Axe");
 
 	UE_DEFINE_GAMEPLAY_TAG(Player_Ability_Equip_Axe, "Player.Ability.Equip.Axe");
 	UE_DEFINE_GAMEPLAY_TAG(Player_Ability_Unequip_Axe, "Player.Ability.Unequip.Axe");
+	UE_DEFINE_GAMEPLAY_TAG(Player_Ability_Light_Attack_Axe, "Player.Ability.Light.Attack.Axe");
+	UE_DEFINE_GAMEPLAY_TAG(Player_Ability_Heavy_Attack_Axe, "Player.Ability.Heavy.Attack.Axe");
 
 	UE_DEFINE_GAMEPLAY_TAG(Player_Weapon_Axe, "Player.Weapon.Axe");
 	UE_DEFINE_GAMEPLAY_TAG(Player_Event_Equip_Axe, "Player.Event.Equip.Axe");
