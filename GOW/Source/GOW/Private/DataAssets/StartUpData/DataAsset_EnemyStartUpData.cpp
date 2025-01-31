eb98d1551b00432ca873b25e220b8865fed25f3a
Povet34 <70003696+Povet34@users.noreply.github.com>
2025-01-31 16:32:05 +0900

    에너미 가디언 StartUP데이터 비동기 로드

--- GOW/Source/GOW/Private/DataAssets/StartUpData/DataAsset_EnemyStartUpData.cpp
+++ GOW/Source/GOW/Private/DataAssets/StartUpData/DataAsset_EnemyStartUpData.cpp
@@ -2,4 +2,28 @@
 
 
 #include "DataAssets/StartUpData/DataAsset_EnemyStartUpData.h"
+#include "AbilitySystem/WarriorAbilitySystemComponent.h"
+#include "AbilitySystem/Abilities/WarriorEnemyGameplayAbility.h"
 
+
+void UDataAsset_EnemyStartUpData::GiveToAbilitySystemComponent(UWarriorAbilitySystemComponent* InASCToGive, int32 ApplyLevel)
+{
+	Super::GiveToAbilitySystemComponent(InASCToGive, ApplyLevel);
+
+	if (!EnemyCombatAbilities.IsEmpty())
+	{
+		for (const TSubclassOf<UWarriorEnemyGameplayAbility>& AbilityClass : EnemyCombatAbilities)
+		{
+			if (!AbilityClass)
+			{
+				continue;
+			}
+
+			FGameplayAbilitySpec Spec(AbilityClass);
+			Spec.SourceObject = InASCToGive->GetAvatarActor();
+			Spec.Level = ApplyLevel;
+
+			InASCToGive->GiveAbility(Spec);
+		}
+	}
+}
